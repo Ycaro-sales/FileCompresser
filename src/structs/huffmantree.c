87098ac470c19dc83eda7a6fbbd7ff114bda641d
ycaro <ycaro00287@gmail.com>
2023-12-18 14:23:36 -0300

    [WIP] feat: huffman tree methods

--- src/structs/huffmantree.c
+++ src/structs/huffmantree.c
@@ -25,7 +25,15 @@ struct huffman_tree {
         hashtable *compressed_table;
         char *tree_string;
 };
-huff_node *create_huffman_node() {}
+huff_node *create_huffman_node(entry *data) {
+        huff_node *tmp = malloc(sizeof *tmp);
+        tmp->character = data->key[0];
+        tmp->frequency = *(int *)data->object;
+        tmp->next = NULL;
+        tmp->left = NULL;
+        tmp->right = NULL;
+        return tmp;
+}
 
 huff_tree *create_huffman_tree(FILE *stream) {
         huff_tree *tmp = malloc(sizeof(huff_tree));
@@ -34,9 +42,10 @@ huff_tree *create_huffman_tree(FILE *stream) {
 
         for (int i = 0; i < char_frequency->capacity; i++) {
                 if (char_frequency->elements[i] != NULL) {
+                        insert_node(tmp, char_frequency->elements[i]->object);
                 }
         }
-        tmp->tree_string = tree_to_string(tmp->root);
+        // tmp->tree_string = tree_to_string(tmp->root);
 
         return tmp;
 }
@@ -47,9 +56,9 @@ unsigned long int hash(const char *letra, size_t size) {
 hashtable *create_char_frequency_table(char *buffer) {
         hashtable *ht = hashtable_create(256, &hash);
         for (int i = 0; i < strlen(buffer); i++) {
-                if (hashtable_lookup(ht->elements, buffer[i]) == NULL) {
-                        huff_node *tmp = create_huffman_node();
-                        hashtable_insert(ht, *buffer[i], tmp);
+                if (hashtable_lookup(ht, &buffer[i]) == NULL) {
+                        huff_node *tmp = create_huffman_node(ht->elements[i]);
+                        hashtable_insert(ht, &buffer[i], tmp);
                 }
         }
 
@@ -61,4 +70,23 @@ static char *get_buffer_from_file(FILE *stream);
 
 static huff_node *merge_nodes(huff_node *left, huff_node *right);
 
-static void insert_node(huff_tree *htree, huff_node *hnode);
+static void insert_node(huff_tree *htree, huff_node *hnode) {
+        huff_node *curr = htree->root;
+        if (curr == NULL) {
+                htree->root = hnode;
+                return;
+        }
+
+        // TODO: sorted insert
+        while (curr->next != NULL) {
+                if (curr->next->frequency == hnode->frequency) {
+                        hnode->next = curr->next;
+                        curr->next = hnode;
+                        return;
+                }
+                else if (curr->next->frequency < hnode->frequency{
+
+                }
+                curr = curr->next;
+        }
+}
